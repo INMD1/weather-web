a8d0284ef80de551f02a01c1f9086ea81af79a17
INMD1 <lyw514549@gmail.com>
2022-09-18 22:16:18 +0900

    리드미 수정 및 ionic icon 존엄성 추가

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
     <!-- 메인 해더 -->
-      <nav class="navbar navbar-expand-lg bg-light">
+      <nav class="navbar navbar-expand-lg">
         <div class="container">
           <div class="row">
-            <div class="col-3">
-              <button class="navbar-toggler" type="button"  data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-                  <span class="navbar-toggler-icon"></span>
+            <div class="mobile col-3 ">
+              <button class="navbar-toggler" type="button"  data-bs-toggle="offcanvas" data-bs-target="#menu_bar" aria-controls="menu_bar">
+                  <ion-icon name="apps-outline"></ion-icon>
               </button>
             </div>
             <div class="col-8">
                   <a class="navbar-brand" href="#">대한민국 해수욕장 날씨</a>
             </div>
           </div>
-          <div class="mobile offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+          <div class="mobile offcanvas offcanvas-start" tabindex="-1" id="menu_bar" aria-labelledby="title_head">
             <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+              <h5 class="offcanvas-title" id="title_head">대한민국 해수욕장 날씨</h5>
               <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
               <div>
-                Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+                여기에는 메뉴가 자세히 나옴니다.
               </div>
               <div class="dropdown mt-3">
                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
