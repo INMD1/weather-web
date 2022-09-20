1cdff943c690d0e38403ece7790d3e1b26324226
INMD1 <lyw514549@gmail.com>
2022-09-20 23:24:55 +0900

    메인 타이틀 제작및 디자인 추가 사이트 아이콘 변경

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <!--바탕화면 삽입-->
   <div class="background-set" :style="{'backgroundImage': 'url(https://raw.githubusercontent.com/INMD1/weather-web/main/src/assets/image/background/bg'+ this.bg_id +'.jpg)'}">
     <!--메인 페이지 -->
+    <nav class="navbar">
+      <div class="container">
+        <a class="navbar-brand MICEGothic" :style="{'color': this.title_color}"  href="#">부산광역시 종합 날씨 정보</a>
+      </div>
+    </nav>
     <div class="container-filed">
       <div class="row">
         <!--현재 온도 또는 습도,풍속,UV 정보를 크게 보여주는 컴포먼트-->
@@ -19,14 +24,23 @@
 
 <script lang="js">
   export default {
+    components : {
+
+    },
     data() {
       return {
-        bg_id : 0
+        bg_id : 0,
+        title_color: "black"
       };
     }, 
     mounted() {
       //이미 랜덤 호출 코드
       this.bg_id = Math.floor(Math.random() * (8 - 1)) + 1;
+
+      //어두운 사진이 나오면 글자 색이 바뀐다.
+      if(this.bg_id == 4 || this.bg_id == 6 || this.bg_id == 7){
+        this.title_color = "white"
+      }
     }
   }
 </script>
