5689521d5a95cd079a1a7ace912cbb6d59f0f165
INMD1 <lyw514549@gmail.com>
2022-09-21 23:18:58 +0900

    기본 디자인틀 완성

--- src/compoment/another_com.vue
+++ src/compoment/another_com.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="row">
-    <div class="blank"></div>
+    <div class="blank">
+      <div class="container-sm">
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+            <img src="" class="card-img-top" alt="">
+            <div class="card-body">
+              <h5 class="card-title">여기에 아이콘이 들어갈 예정</h5>
+              <p class="card-text">그리고 온도를 표시 합니다.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="another_background">
       
     </div>
